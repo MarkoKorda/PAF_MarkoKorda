ed6af5a1d4672c506ff0560f12ef59367f9b17bb
MarkoKorda <mkorda@pmfst.hr>
2021-03-22 12:41:28 +0100

    vjezbe3zad2update

--- Vjezbe3/gibanje.py
+++ Vjezbe3/gibanje.py
@@ -3,3 +3,4 @@ import particle as prt
 p1 = prt.Particle(50,60,70,120)
 print(p1.range(0.01))
 p1.plot_trajectory()
+p1.reset()
--- Vjezbe3/zadatak2.py
+++ Vjezbe3/zadatak2.py
@@ -1,28 +1,60 @@
 import particle as prt
 import matplotlib.pyplot as plt
 
-dtlist = []
-dtmax = 1
-dtlist.append(dtmax)
-n = 100
+def log_scale_ne():
+    dtlist = []
+    dtmax = 1
+    dtlist.append(dtmax)
+    n = 100
 
-for i in range(n):
-    dt_ = dtlist[i]/1.1
-    dtlist.append(dt_)
+    for i in range(n):
+        dt_ = dtlist[i]/1.1
+        dtlist.append(dt_)
 
-re_list = []
+    re_list = []
 
-p = prt.Particle(10,60,40,30)
-
-for i in range(n+1):
-    p.reset()
     p = prt.Particle(10,60,40,30)
-    dt = dtlist[i]
-    re = (abs((p.range(dt)-p.range_analitic())/p.range_analitic()))*100
-    re_list.append(re)
 
-plt.xlabel("dt/s")
-plt.ylabel("relative error")
-plt.xscale("log")
-plt.plot(dtlist,re_list)
-plt.show()
+    for i in range(n+1):
+        p.reset()
+        p = prt.Particle(10,60,40,30)
+        dt = dtlist[i]
+        re = (abs((p.range(dt)-p.range_analitic())/p.range_analitic()))*100
+        re_list.append(re)
+
+    plt.xlabel("dt [s]")
+    plt.ylabel("relative error [%]")
+    plt.xscale("log")
+    plt.plot(dtlist,re_list)
+    plt.show()
+    p.reset()
+
+def lin_scale_ne():
+    dtlist = []
+    dtmin = 0.01
+    dtlist.append(dtmin)
+    n = 100
+
+    for i in range(n):
+        dt_ = dtlist[i] + 0.01
+        dtlist.append(dt_)
+
+    re_list = []
+
+    p = prt.Particle(10,60,40,30)
+
+    for i in range(n+1):
+        p.reset()
+        p = prt.Particle(10,60,40,30)
+        dt = dtlist[i]
+        re = (abs((p.range(dt)-p.range_analitic())/p.range_analitic()))*100
+        re_list.append(re)
+
+    plt.xlabel("dt [s]")
+    plt.ylabel("relative error [%]")
+    plt.plot(dtlist,re_list)
+    plt.show()
+    p.reset()
+
+log_scale_ne()
+lin_scale_ne()
